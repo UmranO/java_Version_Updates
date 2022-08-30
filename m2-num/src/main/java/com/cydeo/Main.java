47e2804dff663840fe0d04b9ea710c4c223ace0e
Umran Ozman <umonal@yahoo.com>
2022-08-29 21:48:22 -0400

    info about Enum object is added

--- m2-num/src/main/java/com/cydeo/Main.java
+++ m2-num/src/main/java/com/cydeo/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("How to access a constant");
 
+        Currency c=Currency.DIME;  // DIME is an object of Currency Enum
+        System.out.println(c);     /* when you print enum object you don't need to put
+                                    toString because toString is already overriden */
+
+
     }
 }
