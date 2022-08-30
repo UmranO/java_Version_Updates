4980f7826cfc67e38b0611e36b3cd67dbaeac95d
Umran Ozman <umonal@yahoo.com>
2022-08-29 21:56:37 -0400

    Enum Class features in an Enum

--- m2-num/src/main/java/com/cydeo/Main.java
+++ m2-num/src/main/java/com/cydeo/Main.java
@@ -5,14 +5,19 @@ public class Main {
         System.out.println("How to access a constant");
 
         Currency c=Currency.DIME;  // DIME is an object of Currency Enum
+                                   // we don't use new key word for object creation in Enum
         System.out.println(c);     /* when you print enum object you don't need to put
                                     toString because toString is already overriden */
 
 
         //How to access all the costants
 
-        Currency[] currencies=Currency.values();  //values() it is also overridden
+        Currency[] currencies=Currency.values();  //values() where is it coming from?
         System.out.println(currencies);
+
+
+    /*Enum can Not extend any class because it internally extends Enum Class
+        that's where toString() is overriden and values() is coming from
 
     }
 }
