1b98a450505a35a691414ff11d7ee102473c1475
Umran Ozman <umonal@yahoo.com>
2022-08-31 16:37:51 -0400

    CreateStream Class is created

---
+++ m6-stream/src/main/java/com/cydeo/CreateStream.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CreateStream {
+    public static void main(String[] args) {
+
+    }
+}
