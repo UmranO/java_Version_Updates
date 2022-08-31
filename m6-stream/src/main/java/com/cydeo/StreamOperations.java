6a3cddf33fa7b54a4ca5d3dd28f8358910c2b804
Umran Ozman <umonal@yahoo.com>
2022-08-31 17:01:48 -0400

    StreamOperations Class is created

---
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamOperations {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        
+
+
+
+    }
+}
