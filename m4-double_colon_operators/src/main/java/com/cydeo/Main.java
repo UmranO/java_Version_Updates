c1f2b60f59389a5731331a45af791a4e0ccf9246
Umran Ozman <umonal@yahoo.com>
2022-08-31 18:24:00 -0400

    Double colon instance () demo is given

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -7,9 +7,19 @@ public class Main {
 
         Calculate s1 = (x,y) -> Calculator.findSum(x,y);
 
+        //Reference to a Static Method
         Calculate s2=Calculator::findSum;
 
         s2.calculate(10,20);
+
+        //Reference to an instance method
+        Calculator obj =new Calculator();
+        Calculate s3= obj::findMultiply;
+
+        //OR
+        Calculate s4=new Calculator() :: findMultiply;
+
+
 
     }
 }
