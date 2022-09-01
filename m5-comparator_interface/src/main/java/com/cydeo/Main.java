a985489fb94bcf3ef35d643beaea42a83dd13c9b
Umran Ozman <umonal@yahoo.com>
2022-09-01 08:25:12 -0400

    Sample Data is created

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+    }
+}
