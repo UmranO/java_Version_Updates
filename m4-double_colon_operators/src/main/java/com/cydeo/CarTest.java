2aa61f877970e65d7803dd1822ad934e1c05f420
Umran Ozman <umonal@yahoo.com>
2022-08-31 22:16:24 -0400

    BiFunction is applied to Two Argument Constructor CarTest Class

--- m4-double_colon_operators/src/main/java/com/cydeo/CarTest.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/CarTest.java
@@ -19,7 +19,12 @@ public class CarTest {
         Function <Integer,Car> func= model-> new Car(model);
         Function <Integer,Car> func1=Car::new;
 
-       
+        //Two Argument Constructor
+
+        BiFunction<String,Integer,Car> b1= (make, model)-> new Car(make,model);
+        BiFunction<String,Integer,Car> b2= Car::new;
+        Car honda =b2.apply("Honda",2015);
+        System.out.println(honda.getMake() +" " + honda.getModel());
     }
 
 }
