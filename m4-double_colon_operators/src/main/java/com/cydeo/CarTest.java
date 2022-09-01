bbf849685cc60f5d8eab115db72fdba4864b6859
Umran Ozman <umonal@yahoo.com>
2022-08-31 21:21:58 -0400

    Supplier is applied to no arg constructor CarTest Class

--- m4-double_colon_operators/src/main/java/com/cydeo/CarTest.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/CarTest.java
@@ -5,7 +5,10 @@ import java.util.function.Supplier;
 public class CarTest {
     public static void main(String[] args) {
 
-      
+        //Zero Argument Constructor
+        Supplier <Car> c1= ()-> new Car();
+        System.out.println(c1.get().getModel());
+
 
     }
 
