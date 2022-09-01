4631f3f620fa565298f01f635cb14b068b6e4f17
Umran Ozman <umonal@yahoo.com>
2022-08-31 21:47:46 -0400

    Function is applied to one arg constructor CarTest Class

--- m4-double_colon_operators/src/main/java/com/cydeo/CarTest.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/CarTest.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class CarTest {
@@ -9,7 +10,13 @@ public class CarTest {
         Supplier <Car> c1= ()-> new Car();
         System.out.println(c1.get().getModel());
 
+        Supplier<Car> c2= Car::new;
+        System.out.println(c2.get().getModel());
 
+        //One Argument Constructor
+
+        Function <Integer,Car> func= model-> new Car(model);
+        Function <Integer,Car> func1=Car::new;
     }
 
 }
