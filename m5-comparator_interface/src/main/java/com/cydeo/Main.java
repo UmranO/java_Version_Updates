a544c7b89039fbe2bed3b6253346082892261077
Umran Ozman <umonal@yahoo.com>
2022-09-01 10:08:13 -0400

    Descending order Implementation of the compare method which is in the Comparator Interface is applied in an example

--- m5-comparator_interface/src/main/java/com/cydeo/Main.java
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -1,12 +1,22 @@
 package com.cydeo;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+
+        //Ascending
+       Collections.sort(list);
+       System.out.println(list);
+
+
+       //Descending
+        Collections.sort(list, new MyComparator());
         System.out.println(list);
     }
 }
