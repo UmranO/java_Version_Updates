6a67d0fbd0c510f088bc2667e5fec418d0460ce6
Umran Ozman <umonal@yahoo.com>
2022-09-01 09:17:03 -0400

    Implementation of the compare method which is in the Comparator Interface is given

--- m5-comparator_interface/src/main/java/com/cydeo/MyComparator.java
+++ m5-comparator_interface/src/main/java/com/cydeo/MyComparator.java
@@ -3,4 +3,8 @@ package com.cydeo;
 import java.util.Comparator;
 
 public class MyComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return (01>02) ? -1 :(01<02) ? 1 :0;
+    }
 }
