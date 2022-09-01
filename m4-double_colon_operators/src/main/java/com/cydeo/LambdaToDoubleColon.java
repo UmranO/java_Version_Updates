d5195909870e5c2bddafcd70ae026b06d6ee1617
Umran Ozman <umonal@yahoo.com>
2022-08-31 22:38:11 -0400

    LambdaToDoubleColon examples given

--- m4-double_colon_operators/src/main/java/com/cydeo/LambdaToDoubleColon.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/LambdaToDoubleColon.java
@@ -5,5 +5,21 @@ public class LambdaToDoubleColon {
     public static void main(String[] args) {
 
 
+        /* (Apple apple)->apple.getWeight()
+            Apple::getWeight;
+            ---------------------------------------
+           ()->Thread.currentThread().dumpStack()
+           Thread.currentThread()::dumpStack()
+           ---------------------------------------
+           (str,i)->str.substring(i)
+           String::substring;
+           ---------------------------------------
+           (String s)->System.out.println(s)
+           Systme.out::println
+
+         */
+
+
+
     }
 }
