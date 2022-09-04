e8c27e98b12fc024af2c04087e5dd20727718c24
Umran Ozman <umonal@yahoo.com>
2022-09-04 07:46:46 -0400

    EmployeeTest Class-stream demo

--- m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
+++ m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
@@ -1,4 +1,14 @@
 package com.cydeo.task;
 
 public class EmployeeTest {
+    public static void main(String[] args) {
+
+        //Print all employee emails
+        System.out.println("Print All Emails");
+        EmployeeData.readAll()
+                // .map(employee->employee.getEmpEmail())
+                   .map(Employee::getEmpEmail)
+                   .forEach(System.out::println);
+
+    }
 }
