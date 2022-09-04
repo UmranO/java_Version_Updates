201bb89d7ea56045dc0465c0620a243b2e455468
Umran Ozman <umonal@yahoo.com>
2022-09-04 13:33:12 -0400

    anyMatch() demo

--- m6-stream/src/main/java/com/cydeo/FindingMatching.java
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -3,11 +3,13 @@ package com.cydeo;
 import com.cydeo.task.DishData;
 
 public class FindingMatching {
+    public static void main(String[] args) {
+
 
 //ALL MATCH
+        System.out.println("allMatch ()");
+        boolean isHealthy=DishData.getAll().stream().allMatch(dish->dish.getCalories()<1000);
+        System.out.println(isHealthy);
 
-    
-
-
-
+    }
 }
