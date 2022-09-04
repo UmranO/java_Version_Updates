8efbe892fe8b1c98e59095685131d9c214691ec5
Umran Ozman <umonal@yahoo.com>
2022-09-04 10:44:21 -0400

    MyComparator class created and updated

--- m5-comparator_interface/src/main/java/com/cydeo/MyComparator.java
+++ m5-comparator_interface/src/main/java/com/cydeo/MyComparator.java
@@ -2,9 +2,10 @@ package com.cydeo;
 
 import java.util.Comparator;
 
-public class MyComparator implements Comparator {
+public class MyComparator implements Comparator<Integer> {
+
     @Override
-    public int compare(Object o1, Object o2) {
-        return (01>02) ? -1 :(01<02) ? 1 :0;
+    public int compare(Integer o1, Integer o2) {
+        return (o1>o2) ? -1 : (o1<o2) ? 1 : 0;
     }
 }
