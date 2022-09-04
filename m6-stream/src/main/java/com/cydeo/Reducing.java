2d2ce34ac118b9a245aaa71a44288ebe844553fa
Umran Ozman <umonal@yahoo.com>
2022-09-04 19:11:44 -0400

    Reducing Class is created

---
+++ m6-stream/src/main/java/com/cydeo/Reducing.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Reducing {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(4, 5, 3, 9);
+
+//        int sum = 0;
+//        for(int number : numbers){
+//            sum = sum + number;
+//        }
+
+    }
+}
