99473556d85458cfb865439de29404d24d38cd9a
Umran Ozman <umonal@yahoo.com>
2022-09-04 22:08:20 -0400

    CollectorsDemo Class is created

---
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -0,0 +1,22 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectorsDemo {
+    public static void main(String[] args) {
+
+
+        List<Integer> numbers = Arrays.asList(3,4,5,6,6,7);
+
+        System.out.println("---------------toCollection(Supplier)-------------------");
+
+        //toCollection(Supplier) : is used to create a Collection using Collector
+        numbers.stream()
+                .filter(p->p%2==0)
+                .collect(CollectorsDemo.toCollection(ArrayList::new));
+
+
+    }
+}
