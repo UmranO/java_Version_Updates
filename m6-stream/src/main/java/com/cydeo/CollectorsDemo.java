5e272a855e3aa7ed10fda97c4768e90638c622f7
Umran Ozman <umonal@yahoo.com>
2022-09-04 22:28:04 -0400

    toCollection () --HashSet demo

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -1,8 +1,6 @@
 package com.cydeo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollectorsDemo {
@@ -14,10 +12,18 @@ public class CollectorsDemo {
         System.out.println("---------------toCollection(Supplier)-------------------");
 
         //toCollection(Supplier) : is used to create a Collection using Collector
+        System.out.println("---------------ArrayList--------------");
+        System.out.println();
         List<Integer>numberList=numbers.stream()
                 .filter(p->p%2==0)
                 .collect(Collectors.toCollection(ArrayList::new));
         System.out.println(numberList);
 
+        System.out.println("---------------Set--------------");
+        System.out.println();
+        Set<Integer> numberSet=numbers.stream()
+                .filter(p->p%2==0)
+                .collect(Collectors.toCollection(HashSet::new));
+        System.out.println(numberSet);
     }
 }
