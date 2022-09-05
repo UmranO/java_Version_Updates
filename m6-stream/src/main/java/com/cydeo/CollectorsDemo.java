7e9223218303ef0ab3ea8acdb5ada19a48e14777
Umran Ozman <umonal@yahoo.com>
2022-09-04 22:14:50 -0400

    toCollection () demo

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -3,6 +3,7 @@ package com.cydeo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectorsDemo {
     public static void main(String[] args) {
@@ -13,10 +14,10 @@ public class CollectorsDemo {
         System.out.println("---------------toCollection(Supplier)-------------------");
 
         //toCollection(Supplier) : is used to create a Collection using Collector
-        numbers.stream()
+        List<Integer>numberList=numbers.stream()
                 .filter(p->p%2==0)
-                .collect(CollectorsDemo.toCollection(ArrayList::new));
-
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(numberList);
 
     }
 }
