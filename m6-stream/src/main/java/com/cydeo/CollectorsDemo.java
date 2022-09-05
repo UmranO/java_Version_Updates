7ce3a40a6fae01f31950607c16c5869d0bc446c0
Umran Ozman <umonal@yahoo.com>
2022-09-04 22:10:06 -0400

    CollectorsDemo Class is updated

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -3,6 +3,7 @@ package com.cydeo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CollectorsDemo {
     public static void main(String[] args) {
