26b181afbf26b4ca9aced43e5a0c3ac5afb0fc8e
Umran Ozman <umonal@yahoo.com>
2022-09-04 22:11:41 -0400

    Revert "CollectorsDemo Class is updated"
    This reverts commit 7ce3a40a6fae01f31950607c16c5869d0bc446c0.

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -3,7 +3,6 @@ package com.cydeo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CollectorsDemo {
     public static void main(String[] args) {
